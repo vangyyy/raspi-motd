833496472bdad40fec62422d55920620e679137b
Rastislav Vangor <rasto.vangor@gmail.com>
2019-09-15 16:39:51 +0200

    Updated time format

--- motd.sh
+++ motd.sh
@@ -5,7 +5,7 @@ read one five fifteen rest < /proc/loadavg
 upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
 
 printf "$(tput setaf 2)
-   .~~.   .~~.    $(date +"%A, %e %B %Y, %r")
+   .~~.   .~~.    $(date +"%A, %d.%m.%Y, %R:%S")
   '. \ ' ' / .'   $(uname -srmo)$(tput setaf 1)
    .~ .~~~..~.    Uptime...............: $(printf "%d days, %02d:%02d:%02d" "$((${upSeconds}/86400))" "$((${upSeconds}/3600%24))" "$((${upSeconds}/60%60))" "$((${upSeconds}%60))")
   : .~.'~'.~. :   Memory...............: $(($(cat /proc/meminfo | grep MemAvailable | awk {'print $2'}) / 1024))M (Available) / $(($(cat /proc/meminfo | grep MemTotal | awk {'print $2'}) / 1024))M (Total)
